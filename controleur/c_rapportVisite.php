83caf5c5c12acfeeaa7cf49ddc3df31d8bb67c0e
Emma <emmaducosmartin@gmail.com>
2025-11-04 08:35:19 +0100

    formulaire de saisie de rapport

---
+++ controleur/c_rapportVisite.php
@@ -0,0 +1,77 @@
+<?php
+if (!isset($_REQUEST['action']) || empty($_REQUEST['action'])) {
+  $action = "voirrapport";
+} else {
+  $action = $_REQUEST['action'];
+}
+switch ($action) {
+  case 'voirrapport': {
+
+
+      $result = getAllrapportDeVisite();
+      include("vues/v_formulaireRapportsDeVisite.php");
+      break;
+    }
+
+
+  case 'afficherrapport': {
+
+
+      if (isset($_REQUEST['rapports']) && getAllInformationRapportDeVisiteNum($_REQUEST['rapports'])) {
+        $rapNum = $_REQUEST['rapports'];
+        $carac = getAllInformationRapportDeVisiteNum($rapNum);
+         if (empty($carac[11])) {
+      $carac[11] = '';// numéro remplacant
+      $carac[12] = ''; // nom remplacant
+      $carac[13] = ''; // prénom remplacant
+    }
+        include("vues/v_afficherRapportDeVisite.php");
+      } else {
+        $_SESSION['erreur'] = true;
+        header("Location: index.php?uc=rapportvisite&action=voirrapport");
+        exit;
+      }
+      break;
+    }
+case 'saisirrapport': {
+    // Pour afficher le formulaire de saisie
+    $motifs = getMotifs();
+    $praticiens = getAllPraticiens(); // Exemple, à adapter à ta fonction
+    $medicaments = getAllMedicaments(); // Exemple, à adapter à ta fonction
+    include("vues/v_saisirRapportDeVisite.php");
+    break;
+}
+
+case 'enregistrerrapport': {
+    session_start();
+    // Récupérer les infos du formulaire (sauf matricule)
+    $numPraticien = $_POST['praticien'];
+    $dateVisite = $_POST['dateVisite'];
+    $motif = $_POST['motif'];
+    $bilan = $_POST['bilan'];
+
+    // Motif "autre" selon la valeur du code
+    $motifAutre = (isset($_POST['motif_autre']) && $motif == 4) ? trim($_POST['motif_autre']) : null;
+
+    // Médocs et remplaçant optionnels
+    $medoc1 = !empty($_POST['medoc1']) ? $_POST['medoc1'] : null;
+    $medoc2 = !empty($_POST['medoc2']) ? $_POST['medoc2'] : null;
+    $numpratremp = !empty($_POST['numRemplacant']) ? $_POST['numRemplacant'] : null;
+    $etat = !empty($_POST['etat']) ? $_POST['etat'] : 1; 
+    $matriculeVisiteur = $_SESSION['matricule'];
+
+    if (addRapportDeVisite($numPraticien, $matriculeVisiteur, $dateVisite, $motif, $motifAutre, $bilan, $medoc1, $medoc2, $etat, $numpratremp)) {
+        $_SESSION['succes'] = 'Rapport bien enregistré !';
+        header('Location: index.php?uc=rapportvisite&action=voirrapport');
+        exit;
+    } else {
+        $_SESSION['erreur'] = 'Erreur lors de l\'enregistrement du rapport';
+        header('Location: index.php?uc=rapportvisite&action=saisirrapport');
+        exit;
+    }
+}
+ default: {
+        header('Location: index.php?uc=rapportvisite&action=voirrapport');
+        exit;
+}
+?>
---
+++ vues/v_formulaireSaisirRapport.php
@@ -0,0 +1,71 @@
+<?php
+
+<form action="index.php?uc=rapportvisite&action=enregistrerrapport" method="post" class="form-group">
+ <label for="praticien">Praticien :</label>
+ <select name="praticien" required class="form-select">
+  <option value="">-- Sélectionner --</option>
+  <?php foreach($praticiens as $praticien) {
+   echo '<option value="'.$praticien['PRA_NUM'].'">'.htmlspecialchars($praticien['PRA_NOM'].' '.$praticien['PRA_PRENOM']).'</option>';
+  } ?>
+ </select> <br><br>
+
+ <label for="dateVisite">Date de visite :</label>
+ <input type="date" name="dateVisite" required class="form-control"><br><br>
+
+ <label for="motif">Motif :</label>
+ <select name="motif" id="motif" required class="form-select" onchange="toggleMotifAutre(this)">
+  <option value="">-- Sélectionner --</option>
+  <?php foreach($motifs as $motif) {
+   $label = htmlspecialchars($motif['MO_LIBELLE']);
+   echo '<option value="'.$motif['MO_CODE'].'">'.$label.'</option>';
+  } ?>
+ </select> <br><br>
+
+ <div id="motifAutreContainer" style="display:none;">
+  <label for="motif_autre">Précisez le motif :</label>
+  <input type="text" name="motif_autre" class="form-control">
+ </div><br>
+
+ <label for="bilan">Bilan :</label>
+ <textarea name="bilan" required class="form-control"></textarea><br><br>
+
+ <label for="medoc1">Médicament présenté 1 (optionnel) :</label>
+ <select name="medoc1" class="form-select">
+  <option value="">Aucun</option>
+  <?php foreach($medicaments as $medoc) {
+   echo '<option value="'.$medoc['MED_DEPOTLEGAL'].'">'.htmlspecialchars($medoc['MED_NOMCOMMERCIAL']).'</option>';
+  } ?>
+ </select><br><br>
+
+ <label for="medoc2">Médicament présenté 2 (optionnel) :</label>
+ <select name="medoc2" class="form-select">
+  <option value="">Aucun</option>
+  <?php foreach($medicaments as $medoc) {
+   echo '<option value="'.$medoc['MED_DEPOTLEGAL'].'">'.htmlspecialchars($medoc['MED_NOMCOMMERCIAL']).'</option>';
+  } ?>
+ </select><br><br>
+
+ <label for="numRemplacant">Remplaçant (optionnel) :</label>
+ <select name="numRemplacant" class="form-select">
+  <option value="">Aucun</option>
+  <?php foreach($praticiens as $praticien) {
+   echo '<option value="'.$praticien['PRA_NUM'].'">'.htmlspecialchars($praticien['PRA_NOM'].' '.$praticien['PRA_PRENOM']).'</option>';
+  } ?>
+ </select><br><br>
+
+ <label for="etat">État du rapport :</label>
+ <select name="etat" class="form-select">
+  <option value="1">Nouveau</option>
+  <option value="2">Clos</option>
+ </select><br><br>
+
+ <input class="btn btn-success" type="submit" value="Enregistrer le rapport">
+</form>
+
+<script>
+function toggleMotifAutre(select) {
+ var container = document.getElementById('motifAutreContainer');
+ container.style.display = (select.value == '4') ? '' : 'none';
+}
+</script>
+?>
